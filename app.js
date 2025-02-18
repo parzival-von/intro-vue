ac1d6ba05e597585c692d51a850e02a1ed1a3e1d
Asier <alumno@gmail.com>
2025-02-18 19:48:11 +0100

    V-for, v-if, v-on

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const {createApp, ref} = Vue;
 
+const sententiae = [
+    { sententia: 'The night is darkest just before the dawn. And I promise you, the dawn is coming.', auctor: 'Harvey Dent, The Dark Knight' },
+    { sententia: 'I believe what doesn’t kill you simply makes you, stranger.', auctor: 'The Joker, The Dark Knight' },
+    { sententia: 'Your anger gives you great power. But if you let it, it will destroy you… As it almost did me', auctor: 'Henri Ducard, Batman Begins' },
+    { sententia: 'You either die a hero or live long enough to see yourself become the villain.', auctor: 'Harvey Dent, The Dark Knight' },
+    { sententia: 'If you’re good at something, never do it for free.', auctor: 'The Joker, The Dark Knight' },
+    { sententia: 'Yes, father. I shall become a bat.', auctor: 'Bruce Wayne/Batman, Batman: Year One' },
+]
+
 const app = createApp({
-    
-    template:`
-    <h1>{{nuntius}}</h1>
-    <p>{{auctor}}</p>
-    `,
-
     setup(){
-        
-        const nuntius = ref("I'am Batman");
-        const auctor = ref('Batman')
 
-        setTimeout(()=>{
-            nuntius.value = 'Soy Goku';
-            auctor.value = 'Goku';
-            console.log(nuntius);
-        },1000);
-        return{
+        const revelareAuctor = ref (true);
 
-            nuntius,
-            auctor,
+        const vectisAuctor = () =>{
+            revelareAuctor.value = !revelareAuctor.value;
         }
 
+        return{
+            sententiae,
+            revelareAuctor,
+            vectisAuctor,
+
+        }
     }
+});
 
-})
-
-app.mount('#myApp')
+app.mount('#myApp');
