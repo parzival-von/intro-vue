2bb75ffb1562129a71268b20d816b469907f67b5
Asier <alumno@gmail.com>
2025-02-18 19:05:30 +0100

    Setup y ref

--- app.js
+++ app.js
@@ -3,10 +3,28 @@ const {createApp, ref} = Vue;
 const app = createApp({
     
     template:`
-    <h1>Hola Mundo</h1>
-    <p>Desde app.json</p>
-    
-    `
+    <h1>{{nuntius}}</h1>
+    <p>{{auctor}}</p>
+    `,
+
+    setup(){
+        
+        const nuntius = ref("I'am Batman");
+        const auctor = ref('Batman')
+
+        setTimeout(()=>{
+            nuntius.value = 'Soy Goku';
+            auctor.value = 'Goku';
+            console.log(nuntius);
+        },1000);
+        return{
+
+            nuntius,
+            auctor,
+        }
+
+    }
+
 })
 
 app.mount('#myApp')
